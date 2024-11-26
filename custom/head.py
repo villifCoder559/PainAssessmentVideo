7db1b63c3978348713c43bf83341dc30028d161d
VillifCoder559 <francesco.villi@gmail.com>
2024-11-26 10:34:43 -0500

    implement confusion matrix for train and test

--- custom/head.py
+++ custom/head.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 import custom.tools as tools
+from torchmetrics.classification import MulticlassConfusionMatrix
 
 # class head:
 #   def __init__(self,head):
@@ -206,160 +207,6 @@ class HeadSVR:
       list_split_indices.append((train_idx,test_idx))
     return grid_search, list_split_indices
   
-  
-# class GRUModel(nn.Module):
-#   def __init__(self, input_size, hidden_size, num_layers, dropout = 0.0, output_size = 1):
-#     super(GRUModel, self).__init__()
-#     print('input_size', input_size)
-#     self.input_size = input_size
-#     self.hidden_size = hidden_size
-#     self.num_layers = num_layers
-#     self.gru = nn.GRU(input_size, hidden_size, num_layers,dropout=dropout, batch_first=True)
-#     self.fc = nn.Linear(hidden_size, output_size)
-  
-#   def forward(self, x):
-#     # x.shape = (batch_size, seq_length, input_size)
-#     out, _ = self.gru(x)
-#     out = self.fc(out[:, -1, :]) # get the last time step output
-#     return out
-  
-# class HeadGRU():
-#   def __init__(self, input_size, hidden_size, num_layers, dropout = 0.0, output_size = 1):
-#     self.model = GRUModel(input_size, hidden_size, num_layers, dropout, output_size)
-  
-#   def start_train_test(self, X_train, y_train, sample_ids_train, subject_ids_train, 
-#                              X_test, y_test, subject_ids_test,
-#                              num_epochs=1, criterion=nn.L1Loss(), optimizer=optim.Adam,lr = 0.001):
-#     # Reshape X_train and X_test in -> (num_samples, seq_length, input_size)
-#     # [nr_videos, nr_windows, T=16, patch_w, patch_h, emb_dim] -> [nr_videos, nr_windows, T=16*patch_w*patch_h*emb_dim]
-    
-#     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], -1)
-#     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], -1)
-#     print(f'sample_ids_train {sample_ids_train}')
-#     uniqe_sample_ids = np.unique(sample_ids_train) # CHECK: sample_ids should be uniqe for each video, no need np.unique
-#     batch_sequence=[]
-#     for id in uniqe_sample_ids:
-#       batch_sequence.append(np.sum(sample_ids_train == id)) # should be a list of 1s
-#     print(f'batch_sequence: {batch_sequence}')
-    
-#     # Get the unique classes
-#     y_train_unique = np.unique(y_train)
-#     y_test_unique = np.unique(y_test)
-#     y_unique = np.unique(np.concatenate((y_train_unique, y_test_unique)))
-
-#     # Get the unique subjects
-#     subject_ids_train_unique = np.unique(subject_ids_train)
-#     subject_ids_test_unique = np.unique(subject_ids_test)
-#     subject_ids_unique = np.unique(np.concatenate((subject_ids_train_unique, subject_ids_test_unique)))
-    
-#     del y_train_unique, y_test_unique, subject_ids_train_unique, subject_ids_test_unique
-
-#     train_losses = []
-#     train_loss_per_class = np.zeros((num_epochs, y_unique.shape[0])) 
-#     train_loss_per_subject = np.zeros((num_epochs, subject_ids_unique.shape[0])) 
-#     test_losses = []
-#     test_loss_per_class = np.zeros((num_epochs, y_unique.shape[0])) 
-#     test_loss_per_subject = np.zeros((num_epochs, subject_ids_unique.shape[0]))
-#     optimizer = optimizer(self.model.parameters(),lr=lr)
-    
-#     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#     print('device GRU:',device)
-#     self.model.to(device)
-#     y_train=y_train.to(device)     # move to gpu to compute the loss
-#     print('y_unique ',y_unique)
-#     y_unique = torch.from_numpy(y_unique).to(device)
-
-#     for epoch in range(num_epochs):
-#       self.model.train()
-#       epoch_loss = 0.0
-#       i = 0
-#       for batch in batch_sequence: # 1 
-#         # Get the batch data
-#         batch_X = X_train[i:i+batch] # shape (batch_size, embed_size)
-#         batch_y = y_train[i:i+batch] # shape (batch_size)  not granted same class for all the batch (UNBC can have differnet classes in the same video)
-#         batch_subject_ids = subject_ids_train[i:i+batch] # shape (batch_size) # same id for all the batch
-#         i += batch
-
-#         # Forward pass
-#         optimizer.zero_grad()
-#         # print(f'batch_X shape {batch_X.shape}')
-#         outputs = self.model(batch_X)
-#         # print(f'outputs_device: {outputs.device} \t batch_y_device: {batch_y.device} \t')
-#         # Compute loss
-#         loss = criterion(outputs, batch_y)
-#         epoch_loss += loss.item()
-
-#         # Backward pass and optimize
-#         loss.backward()
-#         optimizer.step()
-
-#         # Compute loss per class
-#         batch_y_unique = torch.unique(batch_y)
-
-#         for cls in batch_y_unique:
-#           idx = torch.nonzero(batch_y == cls,as_tuple=True)[0]
-#           class_loss = criterion(outputs[idx], batch_y[idx])
-#           idx_y_unique_global = torch.nonzero(cls == y_unique,as_tuple=True)[0]
-#           train_loss_per_class[epoch, idx_y_unique_global.cpu()] = class_loss.detach().cpu().item()
-        
-#         # Compute loss per subject
-#         for id in np.unique(batch_subject_ids):
-#           idx = np.where(batch_subject_ids == id)[0]
-#           subject_loss = criterion(outputs[idx], batch_y[idx])
-
-#           idx_subject_ids_unique_global = np.where(subject_ids_unique == id)
-#           train_loss_per_subject[epoch, idx_subject_ids_unique_global] = subject_loss.detach().cpu().item()
-
-#       # Track the training loss
-#       avg_epoch_loss = epoch_loss / (X_train.size(0) // batch)
-#       train_losses.append(avg_epoch_loss)
-#       self.evaluate(epoch=epoch, X_test=X_test, y_test=y_test, subject_ids_test=subject_ids_test, 
-#                     test_losses=test_losses, test_loss_per_class=test_loss_per_class, 
-#                     test_loss_per_subject=test_loss_per_subject, subject_ids_unique=subject_ids_unique, y_unique=y_unique)
-
-#       print(f'Epoch [{epoch+1}/{num_epochs}]')
-#       print(f'\ttrain_loss: {avg_epoch_loss:.4f} \t test_loss: {test_losses[-1]:.4f}')
-#       print(f'\ttrain_loss_per_class: {train_loss_per_class[epoch]} \t test_loss_per_class: {test_loss_per_class[epoch]}')
-#       print(f'\ttrain_loss_per_subject: {train_loss_per_subject[epoch]} \t test_loss_per_subject: {test_loss_per_subject[epoch]}')
-
-#     return {'train_losses': train_losses, 'train_loss_per_class': train_loss_per_class, 'train_loss_per_subject': train_loss_per_subject, 
-#             'test_losses': test_losses, 'test_loss_per_class': test_loss_per_class, 'test_loss_per_subject': test_loss_per_subject,
-#             'y_unique': y_unique.detach().cpu(), 'subject_ids_unique': subject_ids_unique}
-#     # # 5. Plot the Training Loss
-#     # plt.plot(train_losses)
-#     # plt.xlabel('Epochs')
-#     # plt.ylabel('Training Loss')
-#     # plt.title('Training Loss over Epochs')
-#     # plt.show()  
-      
-#   def evaluate(self, X_test, y_test, subject_ids_test ,test_losses, test_loss_per_class, test_loss_per_subject, epoch, subject_ids_unique, y_unique):
-#       # 6. Evaluate the Model
-#     self.model.eval()
-#     with torch.no_grad():
-#       predictions = self.model(X_test).squeeze().detach().cpu().numpy()
-#       mae = mean_absolute_error(y_test, predictions)
-#       test_losses.append(mae)
-#       y_test = y_test.cpu().numpy()
-#       y_unique = y_unique.cpu().numpy()
-#       # Compute loss per class
-#       for cls in np.unique(y_test):
-#         idx_local = np.where(y_test == cls)[0]
-#         idx_y_unique_global = np.where(y_unique == cls)
-#         print('test_loss_per_class shape ',test_loss_per_class.shape)
-#         print('idx_y_unique_global shape ',idx_y_unique_global)
-#         print('epoch ',epoch)
-#         test_loss_per_class[epoch, idx_y_unique_global] = mean_absolute_error(y_test[idx_local], predictions[idx_local])
-      
-#       # Compute loss per subject
-#       for id in np.unique(subject_ids_test):
-#         idx_local = np.where(subject_ids_test == id)[0]
-#         idx_subject_ids_unique_global = np.where(subject_ids_unique == id)
-#         test_loss_per_subject[epoch, idx_subject_ids_unique_global] = mean_absolute_error(y_test[idx_local], predictions[idx_local])  
-#     # chunk_nr = 0
-#     # for pred, y, id in zip(predictions, y, samples_id):
-#     #   print(f"Video_{id}_{chunk_nr}: Pred. = {pred:.2f}, gt = {y}")
-
-
 class GRUModel(nn.Module):
   def __init__(self, input_size, hidden_size, num_layers, dropout=0.0, output_size=1):
     super(GRUModel, self).__init__()
@@ -384,7 +231,7 @@ class HeadGRU:
   def start_train_test(self, X_train, y_train, subject_ids_train,
                        X_test, y_test, subject_ids_test,
                        num_epochs=10, batch_size=32, criterion=nn.L1Loss(),
-                       optimizer_fn=optim.Adam, lr=0.001):
+                       optimizer_fn=optim.Adam, lr=0.0001):
     # Reshape inputs
     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], -1)
     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], -1)
@@ -416,6 +263,8 @@ class HeadGRU:
     test_loss_per_class = np.zeros((num_epochs, unique_classes.shape[0]))
     train_loss_per_subject = np.zeros((num_epochs, unique_subjects.shape[0]))
     test_loss_per_subject = np.zeros((num_epochs, unique_subjects.shape[0]))
+    train_confusion_matricies = [MulticlassConfusionMatrix(num_classes=unique_classes.shape[0]) for _ in range(num_epochs)]
+    test_confusion_matricies = [MulticlassConfusionMatrix(num_classes=unique_classes.shape[0]) for _ in range(num_epochs)]
 
     for epoch in range(num_epochs):
       self.model.train()
@@ -431,6 +280,7 @@ class HeadGRU:
 
         # Forward pass
         outputs = self.model(batch_X)
+        # outputs = torch.round(outputs)
         loss = criterion(outputs, batch_y)
         epoch_loss += loss.item()
         # Backward pass and optimization
@@ -450,9 +300,10 @@ class HeadGRU:
             subj_idx = np.where(unique_subjects == subj)[0][0]
             subject_loss[subj_idx] += criterion(outputs[mask], batch_y[mask]).item()
             # subject_counts[subj_idx] += mask.sum().item()
+        train_confusion_matricies[epoch].update(outputs.detach().cpu(), batch_y.detach().cpu())
 
-
-      # Normalize class and subject losses
+      train_confusion_matricies[epoch].compute()
+      # Class and subject losses
       train_loss_per_class[epoch] = class_loss 
       train_loss_per_subject[epoch] = subject_loss 
 
@@ -462,19 +313,21 @@ class HeadGRU:
 
       # Evaluate
       avg_test_loss, class_test_loss, subject_test_loss = self.evaluate(
-        test_loader, criterion, device, unique_classes, unique_subjects
+        test_loader, criterion, device, unique_classes, unique_subjects, test_confusion_matricies= test_confusion_matricies[epoch]
       )
+
+      # Save test loss 
       test_losses.append(avg_test_loss)
       test_loss_per_class[epoch] = class_test_loss
       test_loss_per_subject[epoch] = subject_test_loss
-
+      
       print(f'Epoch [{epoch+1}/{num_epochs}] | Train Loss: {avg_train_loss:.4f} | Test Loss: {avg_test_loss:.4f}')
       print(f'\tTrain Loss Per Class: {train_loss_per_class[epoch]}')
       print(f'\tTest Loss Per Class: {test_loss_per_class[epoch]}')
       print(f'\tTrain Loss Per Subject: {train_loss_per_subject[epoch]}')
       print(f'\tTest Loss Per Subject: {test_loss_per_subject[epoch]}')
+
     # Plot losses
-    
     tools.plot_losses(train_losses=train_losses, test_losses=test_losses)
 
     return {
@@ -485,10 +338,12 @@ class HeadGRU:
       'test_loss_per_class': test_loss_per_class,
       'test_loss_per_subject': test_loss_per_subject,
       'y_unique': unique_classes,
-      'subject_ids_unique': unique_subjects
+      'subject_ids_unique': unique_subjects,
+      'train_confusion_matricies': train_confusion_matricies,
+      'test_confusion_matricies': test_confusion_matricies
     }
 
-  def evaluate(self, test_loader, criterion, device, unique_classes, unique_subjects):
+  def evaluate(self, test_loader, criterion, device, unique_classes, unique_subjects, test_confusion_matricies):
     self.model.eval()
     total_loss = 0.0
     class_loss = np.zeros(unique_classes.shape[0])
@@ -517,10 +372,12 @@ class HeadGRU:
           if mask.any():
             subj_idx = np.where(unique_subjects == subj)[0][0]
             subject_loss[subj_idx] += criterion(outputs[mask], batch_y[mask]).item()
+        
+        test_confusion_matricies.update(outputs.detach().cpu(), batch_y.detach().cpu())
 
-    # Normalize class and subject losses
+    # Class and subject losses
     avg_loss = total_loss / len(test_loader)
-    
+    test_confusion_matricies.compute()
     return avg_loss, class_loss, subject_loss
 
 class CrossValidationGRU:
