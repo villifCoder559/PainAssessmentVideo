02d209d5bfa717b87db1a233b2f092f34a1f2309
VillifCoder559 <francesco.villi@gmail.com>
2024-12-02 18:06:16 -0500

    refactor

--- custom/dataset.py
+++ custom/dataset.py
@@ -64,55 +64,46 @@ class customDataset(torch.utils.data.Dataset):
   def __len__(self):
     return len(self.video_labels)
 
-
-  def _generate_csv_subsampled(self, nr_samples_per_class=2):
-    csv_array=self.video_labels.to_numpy()
-    video_labels_columns = self.video_labels.columns.to_numpy()[0].split('\t')
-    # ['subject_id', 'subject_name', 'class_id', 'class_name', 'sample_id', 'sample_name']
-    list_samples=[]
-    for entry in (csv_array):
-      tmp = entry[0].split("\t")
-      list_samples.append(tmp)
-    list_samples = np.stack(list_samples)
-    nr_classes = np.max(list_samples[:,2].astype(int))
-    print(f'number of classes: {nr_classes}, \ntotal number of samples: {nr_samples_per_class*nr_classes}')
-    for cls in range(nr_classes):
-      samples = list_samples[list_samples[:,2].astype(int) == cls]
-      samples = samples[np.random.choice(samples.shape[0], nr_samples_per_class, replace=False), :]
-      if cls == 0:
-        samples_subsampled = samples
-      else:
-        samples_subsampled = np.concatenate((samples_subsampled,samples),axis=0)
-    print(f'samples_subsampled: {samples_subsampled}')
-    save_path = os.path.join('partA','starting_point','subsamples_'+str(nr_samples_per_class)+'_'+str(nr_samples_per_class*nr_classes)+'.csv')
-    subsampled_df = pd.DataFrame(samples_subsampled, columns=video_labels_columns)
-    subsampled_df.to_csv(save_path, index=False, sep='\t')
-    print(f'Subsampled video labels saved to {save_path}')
-  
   def __getitem__(self, idx):
-    # split in # ["subject_id, subject_name, class_id, class_name, sample_id, sample_name"]
-    # print(f'idx: {idx}')
+    """
+    Retrieve a sample from the dataset at the given index.
+    Args:
+      idx (int): Index of the sample to retrieve.
+    Returns:
+      dict: A dictionary containing the following keys:
+        - 'preprocess' (torch.Tensor): Preprocessed video frames with shape 
+          [nr_clips, batch_video=1, clip_length, channels=3, H=224, W=224].
+        - 'labels' (torch.Tensor): Labels for the video frames with shape [nr_clips].
+        - 'subject_id' (torch.Tensor): Subject IDs for the video frames with shape [nr_clips].
+        - 'sample_id' (torch.Tensor): Sample IDs for the video frames with shape [nr_clips].
+        - 'path' (numpy.ndarray): Paths to the video files with shape (nr_clips,).
+        - 'frame_list' (torch.Tensor): Indices of the frames sampled from the video with shape [nr_clips, clip_length].
+    """
+    # split in csv_array ["subject_id, subject_name, class_id, class_name, sample_id, sample_name"]
     csv_array = self.video_labels.iloc[idx,0].split('\t') 
     video_path = os.path.join(self.path_dataset, csv_array[1], csv_array[5])
     video_path += '.mp4'
     container = av.open(video_path)
     tot_frames = container.streams.video[0].frames
+    
     list_indices = self.sample_frame_strategy(tot_frames)
     frames_list=[self._read_video_pyav(container,indices) for indices in list_indices]
     preprocessed_tensors = torch.stack([self.preprocess(list(frames), return_tensors="pt")['pixel_values'] for frames in frames_list])
+    # preprocessed output shape [2, 1, 16, 3, 224, 224] -> [nr_clips, batch_video, clip_length, RGB_channels=3, H=224, W=224]
+    # print(len(frames_list))
     path = np.repeat(video_path, preprocessed_tensors.shape[0])
     
     unit_vector = torch.ones(preprocessed_tensors.shape[0],dtype=torch.int16)    
     sample_id = unit_vector * int(csv_array[4])
     labels = unit_vector * int(csv_array[2])
     subject_id = unit_vector * int(csv_array[0])
-    # print(list_indices.shape)
-    return  {'preprocess':preprocessed_tensors, 
-             'labels': labels, 
-             'subject_id': subject_id, 
-             'sample_id': sample_id, 
-             'path': path, 
-             'frame_list': list_indices}
+
+    return  {'preprocess':preprocessed_tensors, # shape [nr_clips, batch_video=1, clip_length, channels=3, H=224, W=224]
+             'labels': labels,  # torch shape [nr_clips]
+             'subject_id': subject_id, # torch shape [nr_clips]
+             'sample_id': sample_id, # torch shape [nr_clips]
+             'path': path, # np shape (nr_clips,)
+             'frame_list': list_indices} #torch shape [nr_clips, clip_length]
 
   def _read_video_pyav(self,container, indices):
     '''
@@ -136,19 +127,62 @@ class customDataset(torch.utils.data.Dataset):
     return np.stack([x.to_ndarray(format="rgb24") for x in frames])
 
   def _custom_collate_fn(self, batch):
+    """
+    Custom collate function to process and stack batch data for a DataLoader.
+
+    Args:
+      batch (list): A list of dictionaries, where each dictionary contains the following keys:
+        - 'preprocess': Preprocessed video tensor shape [nr_clips, batch_video=1, clip_length=16, channels=3, H=224, W=224]
+        - 'labels': Corresponding labels tensor.
+        - 'path': Path to the video file.
+        - 'subject_id': Subject ID tensor.
+        - 'sample_id': Sample ID tensor.
+        - 'frame_list': List of frames tensor.
+
+    Returns:
+      tuple: A tuple containing:
+        - data (torch.Tensor): Stacked and reshaped video data tensor with shape [nr_clips, channels, clip_length, H, W].
+        - labels (torch.Tensor): Stacked labels tensor with shape [nr_video, nr_clips].
+        - subject_id (torch.Tensor): Stacked subject IDs tensor with shape [nr_video, nr_clips].
+        - sample_id (torch.Tensor): Stacked sample IDs tensor with shape [nr_video, nr_clips].
+        - path (numpy.ndarray): Stacked paths array with shape (nr_video, nr_clips).
+        - list_frames (torch.Tensor): Stacked frame lists tensor with shape [nr_video, nr_clips].
+    """
     # if torch.cuda.is_available():
     #   device = torch.device('cuda')
     # else:
-    device = torch.device('cpu')
-    data = torch.stack([batch[index]['preprocess'].squeeze().transpose(1,2).to(device) for index in range(len(batch))]) #[B,T,C,H,W] ->[B,C,T,H,W]
+    # device = torch.device('cpu')
+    data = torch.stack([batch[index]['preprocess'].squeeze().transpose(1,2) for index in range(len(batch))]) 
+    #data shape: [nr_clips, batch_video=1, clip_length, channels=3, H=224, W=224] -> [nr_clips,clip_length,channels,H,W]
+    data = data.reshape(-1,self.image_channels, self.clip_length, self.image_resize_h, self.image_resize_w)
     labels = torch.stack([batch[index]['labels'] for index in range(len(batch))])
     path = np.stack([batch[index]['path'] for index in range(len(batch))])
     subject_id = torch.stack([batch[index]['subject_id'] for index in range(len(batch))])
     sample_id = torch.stack([batch[index]['sample_id'] for index in range(len(batch))])
     list_frames = torch.stack([batch[index]['frame_list'] for index in range(len(batch))]).squeeze()
-    # print(f'list_frame_collate: {list_frames.shape}')
-    return data.reshape(-1,self.image_channels, self.clip_length, self.image_resize_h, self.image_resize_w), labels, subject_id,\
-           sample_id, path, list_frames
+
+    return data, \
+           labels,\
+           subject_id,\
+           sample_id,\
+           path,\
+           list_frames
+  def get_params_configuration(self):
+    """
+    Returns the configuration parameters of the dataset.
+
+    Returns:
+      dict: A dictionary containing the configuration parameters of the dataset.
+    """
+    return {
+        'path_dataset': self.path_dataset,
+        'path_labels': self.path_labels,
+        'preprocess': self.preprocess.__class__.__name__,
+        'sample_frame_strategy': self.type_sample_frame_strategy.name,
+        'clip_length': self.clip_length,
+        'stride_window': self.stride_window,
+        'stride_inside_window': self.stride_inside_window
+    }  
   
   def _single_uniform_sampling(self, video_len):
     indices = np.linspace(0, video_len-1, self.clip_length, dtype=int)
@@ -201,129 +235,3 @@ class customDataset(torch.utils.data.Dataset):
     # print('Sliding shape', list_indices.shape)
     return list_indices
   
-  # def plot_dataset_distribution_mean_std_duration(self,per_class=False,per_partecipant=False,saving_path=None):
-  #   def plot_distribution(key,title):  
-  #     key_dict = {} # key: subject_id -> value: [duration, N]
-  #     for idx, sample in enumerate(list_samples):
-  #       # csv_array = self.video_labels.iloc[idx,0].split('\t') 
-  #       key_id = sample[key]
-  #       video_path = os.path.join(self.path_dataset, sample[1], sample[5])
-  #       video_path += '.mp4'
-  #       container = av.open(video_path)
-  #       duration_secs = container.streams.video[0].frames // container.streams.video[0].average_rate
-  #       if key_id not in key_dict:
-  #         key_dict[key_id] = []
-  #         key_dict[key_id].append(duration_secs)
-  #     key_id_vector = np.array(list(key_dict.keys())).astype(int)
-  #     mean_duration = np.array([np.mean(key_dict[key]) for key in key_dict.keys()])
-  #     std_duration = np.array([np.std(key_dict[key]) for key in key_dict.keys()])
-  #     indices = key_id_vector.argsort() # TODO: print also elements not availables 
-  #     dataset_name = f'{os.path.split(self.path_labels)[-1].split(".")[0]}'
-  #     plt.figure(figsize=(30, 12))
-  #     plt.bar(key_id_vector[indices].astype(str), mean_duration[indices], yerr=std_duration[indices], color="blue", alpha=0.8)
-  #     plt.xlabel(title,fontsize=16)
-  #     plt.ylabel("mean duration (s)",fontsize=16)
-  #     plt.title(f"Mean Duration per {title} with std ({dataset_name})",fontsize=16)
-  #     plt.xticks(rotation=45,fontsize=13)
-  #     plt.yticks(fontsize=13)
-  #     plt.grid(axis="y", linestyle="--", alpha=0.7)
-  #     # Show the plot
-  #     # plt.tight_layout()
-  #     if saving_path is not None:
-  #       plt.savefig(os.path.join(saving_path,f'{title}_{dataset_name}.png'))
-  #     else:
-  #       plt.show()
-    
-  #   csv_array=self.video_labels.to_numpy() # subject_id, subject_name, class_id, class_name, sample_id, sample_name
-  #   list_samples=[]
-  #   for entry in (csv_array):
-  #     tmp = entry[0].split("\t")
-  #     list_samples.append(tmp)
-  #   list_samples = np.stack(list_samples)
-  #   if per_partecipant is True:
-  #     key = 0
-  #     plot_distribution(key,'participant')
-  #   if per_class is True:
-  #     key = 2
-  #     plot_distribution(key,'class')
-    
-          
-  def save_frames_as_video(self,list_input_video_path, list_frame_indices, output_video_path,all_predictions,list_ground_truth, output_fps=1):
-    """
-    Extract specific frames from a video and save them as a new video.
-
-    :param input_video_path: Path to the original video file.
-    :param frame_indices: List of frame indices to extract and save.
-    :param output_video_path: Path to save the output video.
-    :param output_fps: Frames per second for the output video (default is 30).
-    """
-    # Open the original video
-    out = None
-    # if len(all_predictions.shape)>2:
-    #   all_predictions = all_predictions.reshape(all_predictions.shape[0],-1)
-    # if len(list_ground_truth.shape) :
-    print(f'all_predictions shape: {all_predictions.shape}')
-    # print('all_labels',list_ground_truth)
-    # print(f'output_video_path: {output_video_path}')
-    # print('pred',all_predictions)
-    # print('gt',list_ground_truth)
-    # partA/video/video/112209_m_51_112209_m_51-BL1-086.mp4
-    for i, input_video_path in enumerate(list_input_video_path): # i->[0...33]
-      print(f'input_video_path: {input_video_path}')
-      cap = cv2.VideoCapture(input_video_path)
-      if not cap.isOpened():
-        raise IOError(f"Err: Unable to open video file: {input_video_path}")
-
-      # Get the width and height of the frames in the video
-      frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-      frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-      frame_size = (frame_width, frame_height)
-
-      # Define the codec and create VideoWriter object
-      fourcc = cv2.VideoWriter_fourcc(*'mp4v')  
-      if out is None:
-        out = cv2.VideoWriter(output_video_path, fourcc, output_fps, frame_size)
-      frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-      # print(f'frame_count: {frame_count}')
-      black_frame = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
-      font = cv2.FONT_HERSHEY_SIMPLEX
-      font_scale = 1
-      font_color = (255, 255, 255)
-      thickness = 2
-      count = 0
-      # print(f'frame_indices: {len(list_frame_indices)}')
-      for j,frame_indices in enumerate(list_frame_indices[i]): #j->[0..1] [16]
-        # print('frame_indices',frame_indices) # i->[0,...,32] framle_inidces->[2,16]
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Reset the video capture to the beginning
-        for _ in range(output_fps):
-          number_frame = black_frame.copy()
-          text = os.path.split(input_video_path)[-1]
-          text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-          text_x = (number_frame.shape[1] - text_size[0]) // 2
-          text_y = (number_frame.shape[0] + text_size[1]) // 2
-          cv2.putText(number_frame, text, (text_x, text_y), font, font_scale, font_color, thickness)
-          out.write(number_frame)
-        for frame_idx in range(frame_count):
-          ret, frame = cap.read()
-          if not ret:
-            break
-            # Check if the current frame index is in the list
-          # print(f'frame_indices: {frame_indices}')
-          if frame_idx in frame_indices: # [2,16]
-            # print(f'GT:{list_ground_truth.shape}')
-            # print(f'pred:{all_predictions.shape}')
-            # print(i,j)
-            # print(list_ground_truth[i][j])
-            # print(all_predictions[i][j])
-            pred_rounded = np.round(all_predictions[i][j],2)
-            cv2.putText(frame, str(count)+'/'+str(frame_idx), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,0), thickness, cv2.LINE_AA)
-            cv2.putText(frame, f'gt:{list_ground_truth[i][j]}', (50, 250), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0), thickness, cv2.LINE_AA)
-            cv2.putText(frame, f'pred:{np.round(pred_rounded,2)}', (50, 400), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0), thickness, cv2.LINE_AA)
-            out.write(frame)
-        count+=1
-          # print(f'frame_idx: {frame_idx}')
-    # Release resources
-    cap.release()
-    out.release()
-    print(f"Saved extracted frames to {output_video_path}")
-    # Add 4 black frames at the end
