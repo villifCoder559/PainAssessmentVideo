bdc3878e7c4e2cd8fac5f6700b3d4f32008bb029
villifCoder559 <francesco.villi@gmail.com>
2024-12-17 16:29:40 -0500

    commit

---
+++ extract_feature.py
@@ -0,0 +1,119 @@
+from custom.dataset import customDataset
+from custom.backbone import backbone
+from custom.helper import CLIPS_REDUCTION,EMBEDDING_REDUCTION,MODEL_TYPE,SAMPLE_FRAME_STRATEGY, HEAD
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+import os
+from transformers import AutoImageProcessor
+
+model_type = MODEL_TYPE.VIDEOMAE_v2_S
+pooling_embedding_reduction = EMBEDDING_REDUCTION.MEAN_SPATIAL
+pooling_clips_reduction = CLIPS_REDUCTION.NONE
+sample_frame_strategy = SAMPLE_FRAME_STRATEGY.SLIDING_WINDOW
+
+path_dataset = os.path.join('partA','video','video')
+path_labels = os.path.join('partA','starting_point','samples.csv')
+
+def _extract_features(dataset,path_csv_dataset,batch_size_feat_extraction,backbone):
+  """
+  Extract features from the dataset specified by the CSV file path.
+
+  Args:
+    path_csv_dataset (str): Path to the CSV file containing dataset information.
+    batch_size (int, optional): Number of samples per batch to load. Default is 2.
+
+  Returns:
+    dict: A dictionary containing the following keys:
+      - 'features' (torch.Tensor): shape [n_video * n_clips, temporal_dim=8, patch_h, patch_w, emb_dim].
+      - 'list_labels' (torch.Tensor): shape [n_video * n_clips].
+      - 'list_subject_id' (torch.Tensor): shape (n_video * n_clips).
+      - 'list_sample_id' (torch.Tensor): shape (n_video * n_clips).
+      - 'list_path' (np.ndarray): shape (n_video * n_clips,).
+      - 'list_frames' (torch.Tensor): shape [n_video * n_clips, n_frames].
+
+  """
+  
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
+  print(f"extracting features using.... {device}")
+  list_features = []
+  list_labels = []
+  list_subject_id = []
+  list_sample_id = []
+  list_path = []
+  list_frames = []
+  count = 0
+  dataset.set_path_labels(path_csv_dataset)
+  dataloader = DataLoader(dataset, 
+                          batch_size=batch_size_feat_extraction,
+                          # num_workers=1,
+                          shuffle=False,
+                          collate_fn=dataset._custom_collate_fn)
+  # move the model to the device
+  backbone.model.to(device)
+  backbone.model.eval()
+  with torch.no_grad():
+    # start_total_time = time.time()
+    # start = time.time()
+    for data, labels, subject_id,sample_id, path, list_sampled_frames in dataloader:
+      #############################################################################################################
+      # data shape -> [nr_clips, clip_length=16, channels=3, H=224, W=224]
+      # 
+      # nr_clips  = floor((total_frames-clip_length=16)/stride_window) + 1
+      #           BIOVID -> floor((138-16)/4)) + 1 = 31
+      # 
+      # self.backbone.model ->   85 MB (small_model), 
+      #                         400 MB (base_model), 
+      #                           4 GB (giant_model)
+      # 
+      # video_feat_size [nr_video,8,768] => 8700 * 8 * 768 * 4 = 204 MB
+      #############################################################################################################
+      # print(f'Elapsed time for {batch_size} samples: {time.time() - start}')
+      print(f'data shape {data.shape}')
+      data = data.to(device)
+      with torch.no_grad():
+    # Extract features from clips -> return [B, clips/tubelets, W/patch_w, H/patch_h, emb_dim] 
+        feature = backbone.forward_features(x=data)
+      # feature -> [2, 8, 1, 1, 384]
+      list_frames.append(list_sampled_frames)
+      list_features.append(feature.detach().cpu())
+      list_labels.append(labels)
+      list_sample_id.append(sample_id)
+      list_subject_id.append(subject_id)
+      list_path.append(path)
+      count += 1
+      # if count % 10 == 0:
+      print(f'Batch {count}/{len(dataloader)}')
+      print(f'GPU:\n Free : {torch.cuda.mem_get_info()[0]/1024/1024/1024:.2f} GB \n total: {torch.cuda.mem_get_info()[1]/1024/1024/1024:.2f} GB')
+      del data, feature
+      torch.cuda.empty_cache()
+      # start = time.time()
+  # print(f'Elapsed time for total feature extraction: {time.time() - start_total_time}')
+  # print('Feature extraceton done')
+  backbone.model.to('cpu')
+  # print('backbone moved to cpu')
+  # print(f'torch.cat features {torch.cat(list_features,dim=0).shape}')
+  dict_data = {
+    'features': torch.cat(list_features,dim=0),  # [n_video * n_clips, temporal_dim=8, patch_h, patch_w, emb_dim] 630GB
+    'list_labels': torch.cat(list_labels,dim=0),  # [n_video * n_clips] 8700 * 10 * 4 = 340 KB
+    'list_subject_id': torch.cat(list_subject_id).squeeze(),  # (n_video * n_clips) 8700 * 10 * 4 = 340 KB
+    'list_sample_id': torch.cat(list_sample_id),  # (n_video * n_clips) 8700 * 10 * 4 = 340 KB
+    'list_path': np.concatenate(list_path),  # (n_video * n_clips,) 8700 * 10 * 4 = 340 KB
+    'list_frames': torch.cat(list_frames,dim=0)  # [n_video * n_clips, n_frames] 8700 * 10 * 4 = 340 KB
+  }
+
+  return dict_data 
+
+preprocess = AutoImageProcessor.from_pretrained(os.path.join("local_model_directory","preprocessor_config.json"))
+custom_ds = customDataset(path_dataset=path_dataset,
+                          path_labels=path_labels,
+                          sample_frame_strategy=sample_frame_strategy,
+                          stride_window=12,
+                          preprocess=preprocess,
+                          clip_length=16)
+backbone_model = backbone(model_type=model_type)
+
+dict_data = _extract_features(dataset=custom_ds,
+                              path_csv_dataset=path_labels,
+                              batch_size_feat_extraction=1,
+                              backbone=backbone_model)
