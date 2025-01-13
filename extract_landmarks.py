fa7d511209bd1e8ecb642dfef5b99292c4dba19a
villifCoder559 <francesco.villi@gmail.com>
2025-01-13 23:21:14 +0100

    add save all mean facial landmarks

--- extract_landmarks.py
+++ extract_landmarks.py
@@ -24,18 +24,19 @@ if not os.path.exists(face_ln_saving_folder):
 face_extractor = extractor.FaceExtractor()
 start = time.time()
 count = 0
+all_mean_facial_landmarks = np.zeros((478,3),dtype=np.float32)
 
 for subject_name in list_subject_name:
   folder_path = os.path.join(root_video_path, subject_name)
   all_videos = os.listdir(folder_path)
   list_video_path = [os.path.join(folder_path, video) for video in all_videos]
-  all_mean_facial_landmarks,count_frame=face_extractor.get_mean_facial_landmarks(list_video_path=list_video_path,
+  mean_facial_landmarks,count_frame=face_extractor.get_mean_facial_landmarks(list_video_path=list_video_path,
                                                                                  align=True,
                                                                                  numpy_view=True)
   dict_subject = {'subject_name':subject_name,
-                  'mean_facial_landmarks':all_mean_facial_landmarks,
+                  'mean_facial_landmarks':mean_facial_landmarks,
                   'count_frame':count_frame}
-  
+  all_mean_facial_landmarks += mean_facial_landmarks
   with open(os.path.join(face_ln_saving_folder,subject_name+'.pkl'), 'wb') as f:
     pickle.dump(dict_subject, f)
   total_time = time.time()-start
@@ -47,3 +48,12 @@ for subject_name in list_subject_name:
   print(f'Expected end: {int(expected_time/60/60)} h {int(expected_time/60)%60} m {int(expected_time)%60} s')
   print(f'Total time  : {int(total_time/60/60)} h {int(total_time/60)%60} m {int(total_time)%60} s')
   print('-'*30)
+  
+# Save mean landmarks of all subjects
+all_mean_facial_landmarks /= len(list_subject_name)
+dict_subject = {'mean_facial_landmarks':all_mean_facial_landmarks,
+                'count_subject':len(list_subject_name)}
+with open(os.path.join(face_ln_saving_folder,'all_subjects_mean_landmarks.pkl'), 'wb') as f:
+  pickle.dump(dict_subject, f)
+print(f'All subjects mean landmarks saved in {os.path.join(face_ln_saving_folder,"all_subjects_mean_landmarks.pkl")}')  
+
