40f9e7853889f31ef8baaf586cdda86324cc181b
villifCoder559 <francesco.villi@gmail.com>
2025-03-13 18:40:16 +0100

    fix

--- custom/__init__.py
+++ custom/__init__.py
@@ -1,6 +1,7 @@
-from .backbone import video_backbone
+import custom.backbone  
 # from .build import dataset_builder
-from .dataset import customDataset
-from .helper import *
-from .model import Model_Advanced
-from .tools import *
+import custom.dataset
+import custom.helper 
+import custom.model
+import custom.tools
+import custom.faceExtractor
