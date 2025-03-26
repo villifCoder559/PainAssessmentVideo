77a69f85ef6442f7046b0e96ab0564c8c347f44e
villifCoder559 <francesco.villi@gmail.com>
2025-03-26 17:27:18 +0100

    add dropout

--- jepa/src/models/utils/modules.py
+++ jepa/src/models/utils/modules.py
@@ -86,7 +86,7 @@ class Block(nn.Module):
         mlp_ratio=4.,
         qkv_bias=False,
         qk_scale=None,
-        drop=0.,
+        drop=0., # for MLP and projection layer
         attn_drop=0.,
         act_layer=nn.GELU,
         norm_layer=nn.LayerNorm,
@@ -126,6 +126,8 @@ class CrossAttention(nn.Module):
         dim,
         num_heads=12,
         qkv_bias=False,
+        attn_drop=0.,
+        proj_drop=0.,
         use_sdpa=True
     ):
         super().__init__()
@@ -134,7 +136,9 @@ class CrossAttention(nn.Module):
         self.scale = head_dim ** -0.5
         self.q = nn.Linear(dim, dim, bias=qkv_bias)
         self.kv = nn.Linear(dim, int(dim*2), bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
         self.use_sdpa = use_sdpa
 
     def forward(self, q, x):
@@ -151,9 +155,11 @@ class CrossAttention(nn.Module):
         else:
             xattn = (q @ k.transpose(-2, -1)) * self.scale
             xattn = xattn.softmax(dim=-1)  # (batch_size, num_heads, query_len, seq_len)
+            xattn = self.attn_drop(xattn)
             q = (xattn @ v)
-
         q = q.transpose(1, 2).reshape(B, n, C)
+        q = self.proj(q)
+        q = self.proj_drop(q)
         return q
 
 
@@ -163,19 +169,23 @@ class CrossAttentionBlock(nn.Module):
         dim,
         num_heads,
         mlp_ratio=4.,
+        drop=0.0,
+        attn_drop=0.0,
+        residual_drop=0.0,
         qkv_bias=False,
         act_layer=nn.GELU,
         norm_layer=nn.LayerNorm
     ):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.xattn = CrossAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, use_sdpa=False)
+        self.xattn = CrossAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, use_sdpa=False, attn_drop=attn_drop, proj_drop=drop)
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer)
+        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.residual_drop = nn.Dropout(residual_drop)
 
     def forward(self, q, x):
         y = self.xattn(q, self.norm1(x))
-        q = q + y
-        q = q + self.mlp(self.norm2(q))
+        q = self.residual_drop(q) + y
+        q = self.residual_drop(q) + self.mlp(self.norm2(q))
         return q
