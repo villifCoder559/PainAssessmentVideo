c102ab715d1784a06ba6bf2e23b136708c084bff
villifCoder559 <francesco.villi@gmail.com>
2025-03-26 17:28:04 +0100

    add dropout and positional encoding

--- jepa/src/models/attentive_pooler.py
+++ jepa/src/models/attentive_pooler.py
@@ -9,16 +9,19 @@ import math
 
 import torch
 import torch.nn as nn
+import os
 import sys
-sys.path.append('jepa')
-print(sys.path)
+
+if os.path.abspath('jepa') not in sys.path:
+    sys.path.append(os.path.abspath('jepa'))
+    
 from src.models.utils.modules import (
     Block,
     CrossAttention,
     CrossAttentionBlock
 )
 from src.utils.tensors import trunc_normal_
-# sys.path.remove('jepa')
+from jepa.src.models.utils import pos_embs
 
 class AttentivePooler(nn.Module):
     """ Attentive Pooler """
@@ -31,12 +34,17 @@ class AttentivePooler(nn.Module):
         depth=1,
         norm_layer=nn.LayerNorm,
         init_std=0.02,
+        mlp_dropout=0.0,
+        attn_dropout=0.0,
+        residual_dropout=0.0,
+        pos_enc = False,
         qkv_bias=True,
-        complete_block=True
+        complete_block=True,
     ):
         super().__init__()
         self.query_tokens = nn.Parameter(torch.zeros(1, num_queries, embed_dim))
-
+        self.pos_enc = pos_enc
+        self.pos_enc_tensor = None
         self.complete_block = complete_block
         if complete_block:
             self.cross_attention_block = CrossAttentionBlock(
@@ -44,11 +52,17 @@ class AttentivePooler(nn.Module):
                 num_heads=num_heads,
                 mlp_ratio=mlp_ratio,
                 qkv_bias=qkv_bias,
+                drop=mlp_dropout,
+                attn_drop=attn_dropout,
+                residual_drop=residual_dropout,
                 norm_layer=norm_layer)
         else:
             self.cross_attention_block = CrossAttention(
                 dim=embed_dim,
                 num_heads=num_heads,
+                attn_drop=attn_dropout,
+                proj_drop=mlp_dropout,
+                use_sdpa=False,
                 qkv_bias=qkv_bias)
 
         self.blocks = None
@@ -95,14 +109,18 @@ class AttentivePooler(nn.Module):
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
 
-    def forward(self, x):
+    def forward(self, x): # x: [B, T, C]
+        if self.pos_enc:
+            if self.pos_enc_tensor is None or self.pos_enc_tensor.shape[0] != x.size(1):
+                self.pos_enc_tensor = pos_embs.get_1d_sincos_pos_embed(grid_size=x.size(1), embed_dim=x.size(2), device=x.device.type)
+            x = x + self.pos_enc_tensor
+        
         q = self.query_tokens.repeat(len(x), 1, 1)
         q = self.cross_attention_block(q, x)
         if self.blocks is not None:
             for blk in self.blocks:
                 q = blk(q)
         return q
-
 
 class AttentiveClassifier(nn.Module):
     """ Attentive Classifier """
@@ -113,10 +131,14 @@ class AttentiveClassifier(nn.Module):
         mlp_ratio=4.0,
         depth=1,
         norm_layer=nn.LayerNorm,
+        attn_dropout=0.0,
+        residual_dropout=0.0,
+        dropout_mlp=0.0,
         init_std=0.02,
         qkv_bias=True,
         num_classes=1000,
         complete_block=True,
+        pos_enc=False
     ):
         super().__init__()
         self.num_classes = num_classes
@@ -126,15 +148,21 @@ class AttentiveClassifier(nn.Module):
             num_heads=num_heads,
             mlp_ratio=mlp_ratio,
             depth=depth,
+            mlp_dropout=dropout_mlp,
+            attn_dropout=attn_dropout,
+            residual_dropout=residual_dropout,
             norm_layer=norm_layer,
             init_std=init_std,
             qkv_bias=qkv_bias,
+            pos_enc=pos_enc,
             complete_block=complete_block,
         )
+        self.pos_enc = pos_enc
+        self.pos_enc_tensor = None
         self.linear = nn.Linear(embed_dim, num_classes, bias=True)
         self.linear.reset_parameters()
 
-    def forward(self, x, key_padding_mask=None):
+    def forward(self, x, key_padding_mask=None): # x: [B, T, C]
         x = self.pooler(x).squeeze(1)
         x = self.linear(x)
         return x
