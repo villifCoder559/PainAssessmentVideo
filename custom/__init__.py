d50e4ab429b2a6878cca6bce8da2ae8bbb301305
villifCoder559 <francesco.villi@gmail.com>
2025-05-26 09:48:30 +0200

    commit

--- custom/__init__.py
+++ custom/__init__.py
@@ -1,7 +1,7 @@
-# import custom.backbone  
-# # from .build import dataset_builder
-# import custom.dataset
-# import custom.helper 
-# import custom.model
-# import custom.tools
-# import custom.faceExtractor
+import custom.backbone  
+# from .build import dataset_builder
+import custom.dataset
+import custom.helper 
+import custom.model
+import custom.tools
+import custom.faceExtractor
