d9b2fc1c2c1a2ebaeaa9bbbb0fb5ad8d283f2236
villifCoder559 <francesco.villi@gmail.com>
2025-05-26 09:52:29 +0200

    add interpolation

--- extract_video_frontalized.py
+++ extract_video_frontalized.py
@@ -14,7 +14,7 @@ def load_reference_landmarks(path):
   ref_landmarks = ref_landmarks['mean_facial_landmarks']
   return ref_landmarks
 
-def main(generate_video,csv_path,path_folder_output,list_target_video,path_ref_landmarks,global_path,from_,to_,video_folder_path,log_error_path=None,only_oval=False):
+def main(generate_video,csv_path,path_folder_output,list_target_video,path_ref_landmarks,global_path,from_,to_,interpolation_mod_chunk,video_folder_path,align_before_front,log_error_path=None,only_oval=False):
   
   face_extractor = extractor.FaceExtractor(visionRunningMode='video')
   csv_list_video_path = np.array(tools.get_list_video_path_from_csv(csv_path=csv_path,
@@ -49,6 +49,8 @@ def main(generate_video,csv_path,path_folder_output,list_target_video,path_ref_l
       dict_result_frontalization = face_extractor.frontalized_video(video_path=video_path,
                                                                     ref_landmarks=ref_landmarks,
                                                                     only_landmarks_crop=only_oval,
+                                                                    align_before_front=align_before_front,
+                                                                    interpolation_mod_chunk=interpolation_mod_chunk, # in order to get a multiple of the input number (ex: videoMae chunk is 16, so we need to set this to 16)
                                                                     extra_landmark_smoothing=None)
     except extractor.DetectionError as e:
       print(f'Error in {video_path}: {e}')
@@ -63,7 +65,7 @@ def main(generate_video,csv_path,path_folder_output,list_target_video,path_ref_l
       out_path = os.path.join(path_folder_output,folder_id,sample_id)
       if global_path:
         out_path = GLOBAL_PATH.get_global_path(out_path)
-        
+      print(f'length of list frame: {len(dict_result_frontalization["list_frontalized_frame"])}')
       tools.generate_video_from_list_frame(list_frame=dict_result_frontalization['list_frontalized_frame'],
                                           path_video_output=out_path)
       print(f'Video frontalized: {out_path}')
@@ -105,7 +107,8 @@ if __name__ == '__main__':
   # parser.add_argument('--log_er_p', type=str, default=os.path.join('partA','video','video_frontalized'), help='Path to log error file')
   parser.add_argument('--vfp', type=str, default=os.path.join('partA','video','video'), help='Path to video folder. Default to partA/video/video')
   parser.add_argument('--only_oval', action='store_true', help='Extract only face oval from the video')
-  parser
+  parser.add_argument('--interpolation_mod_chunk',nargs='*', default=None, help='mod can be spread_linearly or mirror_start_video, chunk is the chunk size in order to get a multiple number of frames according to chunk')
+  parser.add_argument('--align_before_front', action='store_true', help='Align the face before frontalization')
   # sripts example: python3 extract_video_frontalized.py --g_path --gv --csv partA/starting_point/samples_exc_no_detection.csv --pfo partA/video/video_frontalized_new --prl partA/video/mean_face_landmarks_per_subject/all_subjects_mean_landmarks.pkl --from_ 0 --to_ 10
   args = parser.parse_args()
   if args.ltv and args.ltv[0].endswith('.txt'):
@@ -113,6 +116,11 @@ if __name__ == '__main__':
     
   if args.g_path:
     set_global_args(args)
+  dict_args = vars(args)
+  if dict_args['interpolation_mod_chunk'] and len(dict_args['interpolation_mod_chunk']) != 2:
+    print(f'interpolation_mod_chunk must have 2 elements. mod can be spread_linearly or mirror_start_video, chunk is the chunk size in order to get a multiple number of frames according to chunk')
+  for key, value in dict_args.items():
+    print(f'{key}: {value}')
     
   main(generate_video=args.gv,
        csv_path=args.csv,
@@ -124,6 +132,8 @@ if __name__ == '__main__':
        to_=args.to_,
        log_error_path=args.pfo,
        only_oval=args.only_oval,
+       align_before_front=args.align_before_front,
+       interpolation_mod_chunk=[args.interpolation_mod_chunk[0],int(args.interpolation_mod_chunk[1])] if args.interpolation_mod_chunk else None,
        video_folder_path=args.vfp)
   
   
