2b171f340f814b58f24a0dc550d5623d6c076cfa
villifCoder559 <francesco.villi@gmail.com>
2025-05-26 09:51:15 +0200

    add mask and return cross-attention values

--- jepa/src/models/utils/modules.py
+++ jepa/src/models/utils/modules.py
@@ -58,20 +58,25 @@ class Attention(nn.Module):
         self.proj_drop = nn.Dropout(proj_drop)
         self.use_sdpa = use_sdpa
 
-    def forward(self, x, mask=None):
+    def forward(self, x, mask=None): # x: [B, N, C], mask: [B, N]
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]  # [B, num_heads, N, D]
-
+        if mask is not None:
+            if mask.ndim == 2:
+                mask = mask[:, None, None,:].expand(B, self.num_heads, N, N)
         if self.use_sdpa:
             with torch.nn.attention.sdpa_kernel(backends=torch.nn.attention.SDPBackend.EFFICIENT_ATTENTION):
                 
                 x = F.scaled_dot_product_attention(q, k, v, 
                                                    dropout_p=self.attn_drop_value if self.training else 0.0,
+                                                   attn_mask=mask, # True -> consider to compute attention, False -> NOT consider to compute
                                                    scale=self.scale)
                 attn = None
         else:
             attn = (q @ k.transpose(-2, -1)) * self.scale  # [B, num_heads, D, D]
+            if mask:
+                attn = attn.masked_fill(~mask, float('-inf'))
             attn = attn.softmax(dim=-1)
             attn = self.attn_drop(attn)
             x = (attn @ v)
@@ -114,7 +119,7 @@ class Block(nn.Module):
             act_layer=act_layer,
             drop=drop)
 
-    def forward(self, x, return_attention=False, mask=None):
+    def forward(self, x, mask=None, return_attention=False):
         y, attn = self.attn(self.norm1(x), mask=mask)
         if return_attention:
             return attn
@@ -145,28 +150,33 @@ class CrossAttention(nn.Module):
         self.proj_drop = nn.Dropout(proj_drop)
         self.use_sdpa = use_sdpa
 
-    def forward(self, q, x):
+    def forward(self, q, x,mask=None,return_xattn=False): # q: [B, n, C], x: [B, N, C], mask: [B, N]
         B, n, C = q.shape
         q = self.q(q).reshape(B, n, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3) # (batch_size, num_heads, query_len, feature_dim_per_head)
-
         B, N, C = x.shape
         kv = self.kv(x).reshape(B, N, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         k, v = kv[0], kv[1]  # (batch_size, num_heads, seq_len, feature_dim_per_head)
-
-        if self.use_sdpa:
+        if mask is not None:
+            if mask.ndim == 2:
+                mask = mask[:, None, None,:].expand(B, self.num_heads, n, N)
+        if self.use_sdpa and not return_xattn:
             with torch.nn.attention.sdpa_kernel(backends=torch.nn.attention.SDPBackend.EFFICIENT_ATTENTION):
                 q = F.scaled_dot_product_attention(q, k, v,
                                                    dropout_p=self.attn_drop_value if self.training else 0.0,
+                                                   attn_mask=mask, # True -> consider to compute attention, False -> NOT consider to compute
                                                    scale=self.scale)
+                xattn = None
         else:
             xattn = (q @ k.transpose(-2, -1)) * self.scale
+            if mask is not None:
+                xattn = xattn.masked_fill(~mask, float('-inf'))
             xattn = xattn.softmax(dim=-1)  # (batch_size, num_heads, query_len, seq_len)
             xattn = self.attn_drop(xattn)
             q = (xattn @ v)
         q = q.transpose(1, 2).reshape(B, n, C)
         q = self.proj(q)
         q = self.proj_drop(q)
-        return q
+        return q, xattn
 
 
 class CrossAttentionBlock(nn.Module):
@@ -185,14 +195,19 @@ class CrossAttentionBlock(nn.Module):
     ):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.xattn = CrossAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, use_sdpa=use_sdpa, attn_drop=attn_drop, proj_drop=drop)
+        self.xattn = CrossAttention(dim, 
+                                    num_heads=num_heads,
+                                    qkv_bias=qkv_bias,
+                                    use_sdpa=use_sdpa,
+                                    attn_drop=attn_drop,
+                                    proj_drop=drop)
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
         self.residual_drop = nn.Dropout(residual_drop)
 
-    def forward(self, q, x):
-        y = self.xattn(q, self.norm1(x))
+    def forward(self, q, x, mask=None, return_xattn=False):
+        y,xattn = self.xattn(q, self.norm1(x), mask=mask, return_xattn=return_xattn)
         q = self.residual_drop(q) + y
         q = self.residual_drop(q) + self.mlp(self.norm2(q))
-        return q
+        return q,xattn
