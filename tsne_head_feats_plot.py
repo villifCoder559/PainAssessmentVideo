0ff2144d3bce375d6908d93dee09763d9a128013
villifCoder559 <francesco.villi@gmail.com>
2025-06-27 09:38:38 +0200

    fix

--- tsne_head_feats_plot.py
+++ tsne_head_feats_plot.py
@@ -8,11 +8,11 @@ import pickle
 
 
 feat_path = "history_run_samples_0_4_selftrained_ce_1_1_1_stride4_L1_366069_ATTENTIVE_JEPA_villi-Inspiron-16-Plus-7620_1750673405/1750673406842_VIDEOMAE_v2_G_NONE_NONE_SLIDING_WINDOW_ATTENTIVE_JEPA/train_ATTENTIVE_JEPA/k0_cross_val/k0_cross_val_sub_0/head_features_train.safetensors"
+group_key = 'subjects' # 'labels' or 'subjects'
 
 dict_data = tools.load_dict_data(saving_folder_path=feat_path)
 tsne_feats, config, _, _ = tsne_cuda_tools.compute_tsne(X=dict_data['features'])
 
-group_key = 'subjects' # 'labels' or 'subjects'
 group_by = dict_data[group_key]
 unique_groups = np.unique(group_by)
 plot_path = os.path.join(os.path.dirname(feat_path), f'{os.path.basename(feat_path)}_{group_key}.png')
@@ -50,7 +50,7 @@ elif group_key == 'subjects':
   n_batches = len(batch_groups)
   n_cols = min(4, n_batches)  # Max 4 columns
   n_rows = (n_batches + n_cols - 1) // n_cols  # Ceiling division
-  fig_size = (max(n_cols * 5, 30), max(n_rows * 4, 30))  # Each subplot is 5x5 inches
+  fig_size = (min(n_cols * 5, 30), min(n_rows * 4, 30))  # Each subplot is 5x5 inches
   
   fig, axes = plt.subplots(n_rows, n_cols, figsize=(20, 20))
   
@@ -63,15 +63,16 @@ elif group_key == 'subjects':
     axes = axes.reshape(-1, 1)
   
   # Plot each batch
+  
   for batch_idx, batch in enumerate(batch_groups):
     row = batch_idx // n_cols
     col = batch_idx % n_cols
     
-    for group in batch:
+    for i,group in enumerate(batch):
       indices = np.where(group_by == group)[0]
       axes[row, col].scatter(tsne_feats[indices, 0], tsne_feats[indices, 1], 
                             label=f"Subject {group}", 
-                            color=colors(batch_idx), alpha=0.5)
+                            color=colors(i), alpha=0.5)
     
     axes[row, col].set_title(f'Subjects Batch {batch_idx + 1}')
     axes[row, col].set_xlabel('t-SNE Component 1')
@@ -88,5 +89,7 @@ elif group_key == 'subjects':
   plt.tight_layout()
   plt.savefig(plot_path)
   plt.close(fig)
+  
+print(f"t-SNE plot saved at: {plot_path}")
     
     
