c8f17f365d48647681f7f92773ca9d0c94c9fcb0
villifCoder559 <francesco.villi@gmail.com>
2025-09-12 15:34:37 +0200

    fix

--- convert_whole_as_aggregated.py
+++ convert_whole_as_aggregated.py
@@ -6,7 +6,7 @@ import tqdm
 import custom.helper as helper
 import safetensors.torch
 
-root_dataset_folder = "/equilibrium/fvilli/PainAssessmentVideo/partA/video/features/VideoMaeV2_S/features_BIOVID_S_stride_32_inside_2_last143_combined_float16"
+root_dataset_folder = "/equilibrium/fvilli/PainAssessmentVideo/partA/video/features/VideoMaeV2_S/whole_features_BIOVID_S_stride_32_inside_2_shifted_last143"
 print(f"Root dataset folder: {root_dataset_folder}")
 list_path_safetensor = []
 for root, dirs, files in os.walk(root_dataset_folder):
@@ -15,13 +15,13 @@ for root, dirs, files in os.walk(root_dataset_folder):
       list_path_safetensor.append(os.path.join(root,file))
 print(f"Number of safetensors found: {len(list_path_safetensor)}")
 
-new_path = "/equilibrium/fvilli/PainAssessmentVideo/partA/video/features/VideoMaeV2_S/features_BIOVID_S_stride_32_inside_2_last143_combined_float16_partial_aggregated"
+new_path = "/equilibrium/fvilli/PainAssessmentVideo/partA/video/features/VideoMaeV2_S/whole_features_BIOVID_S_stride_32_inside_2_shifted_last143_aggregated"
 new_dict_data = {}
 count = 0
 for path_safetensor in tqdm.tqdm(list_path_safetensor,desc="Converting to float16..."):
   # Load data
   data = tools.load_dict_data(path_safetensor)
-  original_sample_id = min(data['list_sample_id'] - helper.get_shift_for_sample_id('bottom_left'))
+  original_sample_id = data['list_sample_id'][0]
   if original_sample_id <= 0 or original_sample_id > 8700:
     raise ValueError(f"Original sample id {original_sample_id} is out of range [1, 8700]")
   
@@ -32,7 +32,10 @@ for path_safetensor in tqdm.tqdm(list_path_safetensor,desc="Converting to float1
       new_dict_data[k] = []
     new_dict_data[k].append(v)
   count += 1
-  
+
+print(f"Number of aggregated safetensors: {count}")
+safetensors.torch.save_file({f'{k}':torch.concat(v, dim=0) for k,v in new_dict_data.items()}, new_path+'.safetensors')
+print(f"Saved aggregated safetensors to {new_path+'.safetensors'}")
   # # Save partial to avoid system kill due to large memory usage
   # dict_size_GB = sum(
   #     vv.element_size() * vv.nelement()
@@ -49,9 +52,9 @@ for path_safetensor in tqdm.tqdm(list_path_safetensor,desc="Converting to float1
   #   new_dict_data = {}
     
 # Save remaining data
-if len(new_dict_data) > 0:
-  for k,v in tqdm.tqdm(new_dict_data.items(), desc="Concatenating tensors..."):
-    partial_path = os.path.join(new_path, f"aggregated_{k}.safetensors")
-    safetensors.torch.save_file({f'{k}':torch.concat(v, dim=0)}, partial_path)
-    # tools.save_dict_data(torch.concat(v, dim=0), partial_path, True)
-    del new_dict_data[k]
+# if len(new_dict_data) > 0:
+#   for k,v in tqdm.tqdm(new_dict_data.items(), desc="Concatenating tensors..."):
+#     partial_path = os.path.join(new_path, f"aggregated_{k}.safetensors")
+#     safetensors.torch.save_file({f'{k}':torch.concat(v, dim=0)}, partial_path)
+#     # tools.save_dict_data(torch.concat(v, dim=0), partial_path, True)
+#     del new_dict_data[k]
