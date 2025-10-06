83d31cf04e6a41a992fe1217e0dad0d5140b2e33
villifCoder559 <francesco.villi@gmail.com>
2025-10-06 19:40:16 +0200

    add caer

--- quadrants_merge_feats.py
+++ quadrants_merge_feats.py
@@ -10,8 +10,15 @@ import argparse
 parser = argparse.ArgumentParser() 
 parser.add_argument('--root_folder', type=str, required=True, help='Path to the root folder containing quadrant features')
 parser.add_argument('--combined_root_folder', type=str, default=None, help='Path to the output folder for combined features')
-root_folder = parser.parse_args().root_folder
-combined_root_folder = parser.parse_args().combined_root_folder
+parser.add_argument('--csv_path', type=str, default=None, help='Path to the CSV file with sample and subject names')
+args = parser.parse_args()
+
+csv_path = args.csv_path
+if args.csv_path is None:
+  args.csv_path = "partA/starting_point/samples.csv"
+  
+root_folder = args.root_folder
+combined_root_folder = args.combined_root_folder
 if combined_root_folder is None:
   if root_folder.endswith('/'):
     root_folder = root_folder[:-1]
@@ -24,11 +31,15 @@ if root_folder == combined_root_folder:
   raise ValueError("Root folder and combined root folder must be different")
 
 quadrants_id = ['upper_left', 'upper_right', 'bottom_left', 'bottom_right']
-csv_path = "partA/starting_point/samples.csv"
 
-df = pd.read_csv(csv_path, sep='\t')
+df = pd.read_csv(csv_path, sep='\t', dtype={'subject_name': str, 'sample_name': str})
 list_sample_name = df['sample_name'].to_list()
 list_subject_name = df['subject_name'].to_list()
+if 'caer' in csv_path.lower():
+  #remove train from list_subject_name
+  list_subject_name = [s.replace('train/', '') for s in list_subject_name]
+  list_subject_name = [s.replace('validation/', '') for s in list_subject_name]
+  list_subject_name = [s.replace('test/', '') for s in list_subject_name]
 zip_subject_sample = list(zip(list_subject_name, list_sample_name))  # materialize list for executor
 
 
@@ -39,7 +50,7 @@ def process_sample(name, sample):
   }
 
   if not all(os.path.exists(p) for p in feats_quadrant_path.values()):
-    raise FileNotFoundError(f"Features not found for sample {sample} in subject {name} in {q} quadrant")
+    raise FileNotFoundError(f"Features not found in path: {feats_quadrant_path}")
 
   dict_feats = {q: tools.load_dict_data(feats_quadrant_path[q]) for q in quadrants_id}
   keys = list(dict_feats[quadrants_id[0]].keys())
@@ -51,6 +62,7 @@ def process_sample(name, sample):
 
   for k in keys:
     first = dict_feats[quadrants_id[0]][k]
+    
     if isinstance(first, np.ndarray):
       combined_feats[k] = np.concatenate([dict_feats[q][k] for q in quadrants_id], axis=0)
     elif isinstance(first, torch.Tensor):
