dcce783a314c1803688ba284feb33b7da06feae6
villifCoder559 <francesco.villi@gmail.com>
2025-10-06 19:40:03 +0200

    add paralle process

--- extract_video_frontalized.py
+++ extract_video_frontalized.py
@@ -1,155 +1,232 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import pickle
+from pathlib import Path
+import numpy as np
+import tqdm
+import multiprocessing as mp
+from multiprocessing import Manager
+
 import custom.faceExtractor as extractor
 import custom.tools as tools
-import os 
-import pickle
-import time 
-import argparse
-import numpy as np
-from pathlib import Path
 from custom.helper import GLOBAL_PATH
-import numpy as np
+
+# Global variables initialized in worker initializer
+face_extractor = None
+worker_config = None
+log_lock = None
 
 def load_reference_landmarks(path):
-  ref_landmarks = pickle.load(open(path, 'rb'))
-  ref_landmarks = ref_landmarks['mean_facial_landmarks']
-  return ref_landmarks
+  with open(path, 'rb') as f:
+    ref_landmarks = pickle.load(f)
+  return ref_landmarks['mean_facial_landmarks']
 
-def main(generate_video,csv_path,path_folder_output,list_target_video,path_ref_landmarks,global_path,from_,to_,interpolation_mod_chunk,video_folder_path,align_before_front,log_error_path=None,only_oval=False):
+def worker_init(ref_landmarks, cfg, shared_dict):
+  """
+  Called once per worker process. Creates a FaceExtractor for worker.
+  """
+  global face_extractor, worker_config, _dict
+  _dict = shared_dict
+  worker_config = cfg
+  face_extractor = extractor.FaceExtractor(
+    visionRunningMode='video',
+    min_face_detection_confidence=0.3,
+    min_face_presence_confidence=0.3,
+    min_tracking_confidence=0.3
+  )
+  worker_config['ref_landmarks'] = ref_landmarks
+
+def process_one_video(video_path):
+  """
+  Process a single video. Runs inside worker process.
+  """
+  global face_extractor, worker_config, _dict
+  out = {'video': video_path, 'ok': False, 'error': None}
+
+  try:
+    original_video_path = video_path
+    if worker_config.get('global_path'):
+      video_path = GLOBAL_PATH.get_global_path(video_path)
+
+    sample_id = os.path.split(video_path)[-1]
+    folder_id = Path(video_path).parts[-2]
+    out_path = os.path.join(worker_config['path_folder_output'], folder_id, sample_id)
+    if worker_config.get('global_path'):
+      out_path = GLOBAL_PATH.get_global_path(out_path)
+
+    dict_result = face_extractor.frontalized_video(
+      video_path=original_video_path,
+      ref_landmarks=worker_config['ref_landmarks'],
+      only_landmarks_crop=worker_config['only_oval'],
+      align_before_front=worker_config['align_before_front'],
+      interpolation_mod_chunk=worker_config['interpolation_mod_chunk'],
+      extra_landmark_smoothing=None
+    )
+
+    if worker_config.get('generate_video'):
+      tools.generate_video_from_list_frame(
+        list_frame=dict_result['list_frontalized_frame'],
+        fps=dict_result['FPS'],
+        path_video_output=out_path
+      )
+
+    out['ok'] = True
+    return out
+
+  except extractor.DetectionError as e:
+    msg = f'{video_path}: {e}'
+    _dict[f'error_{os.getpid()}'] = e.list_no_detection_idx
+    out['error'] = str(e)
+    return out
   
-  face_extractor = extractor.FaceExtractor(visionRunningMode='video',
-                                           min_face_detection_confidence=0.3,
-                                           min_face_presence_confidence=0.3,
-                                           min_tracking_confidence=0.3)
-  csv_list_video_path = np.array(tools.get_list_video_path_from_csv(csv_path=csv_path,
-                                                                    video_folder_path=video_folder_path))
+  except Exception as e:
+    _dict[f'error_{os.getpid()}'] = str(e)
+    out['error'] = repr(e)
+    return out
+
+def prepare_video_list(csv_path, video_folder_path, list_target_video, from_, to_):
+  
+  csv_list_video_path = np.array(
+    tools.get_list_video_path_from_csv(csv_path=csv_path,
+                                       video_folder_path=video_folder_path)
+  )
   root_video_path = os.path.split(os.path.split(csv_list_video_path[0])[0])[0]
-  list_video_path = []
-  # Init list_video_path with target video
+
   if list_target_video is not None:
-    list_target_video = np.array([os.path.join(root_video_path,video.split('-')[0],video) for video in list_target_video])
+    list_target_video = np.array([os.path.join(root_video_path, v.split('-')[0], v)
+                                  for v in list_target_video])
+    list_video_path = []
     for video in list_target_video:
-      # folder_sample = 
       mask = (csv_list_video_path == video)
-      list_video_path.append(csv_list_video_path[mask][0])
+      if np.any(mask):
+        list_video_path.append(csv_list_video_path[mask][0])
   else:
-      list_video_path = csv_list_video_path[from_:to_]
+    list_video_path = csv_list_video_path[from_:to_]
+
+  return list_video_path
+
+def main(generate_video, csv_path, path_folder_output, list_target_video,
+         path_ref_landmarks, global_path, from_, to_, interpolation_mod_chunk,
+         video_folder_path, align_before_front, log_error_path=None,
+         only_oval=False, workers=4):
+
+  csv_path = os.path.expanduser(csv_path)
+  path_folder_output = os.path.expanduser(path_folder_output)
+  if log_error_path is None:
+    log_error_path = os.path.join(path_folder_output, 'frontalize_errors.log')
+
+  ref_landmarks = load_reference_landmarks(path_ref_landmarks)
+  list_video_path = prepare_video_list(csv_path, video_folder_path,
+                                       list_target_video, from_, to_)
+
+  if len(list_video_path) == 0:
+    print("No videos to process. Exiting.")
+    return
+
   print(f'\n csv path: {csv_path}\n')
   print(f'first video path: {list_video_path[0]}')
   print(f'last video path: {list_video_path[-1]}')
   print(f'Total video: {len(list_video_path)}\n')
-  count = 0
-  # '/media/villi/TOSHIBA EXT/orig_video/video/video/082414_m_64/082414_m_64-BL1-088.mp4'    
-  # partA/video/video_frontalized/101514_w_36/101514_w_36-PA2-035.mp4
-  # list_video_path = ['/media/villi/TOSHIBA EXT/orig_video/video/video/082814_w_46/082814_w_46-PA3-049.mp4']
-  ref_landmarks = load_reference_landmarks(path=path_ref_landmarks)
-  start = time.time()
-  for video_path in list_video_path:
-    print('Frontalizing video: ',video_path)
-    if global_path:
-      video_path = GLOBAL_PATH.get_global_path(video_path)
-    try:
-      # extra_landmark_smoothing = extractor.LandmarkSmoother(method='moving_average', window_size=5)
-      dict_result_frontalization = face_extractor.frontalized_video(video_path=video_path,
-                                                                    ref_landmarks=ref_landmarks,
-                                                                    only_landmarks_crop=only_oval,
-                                                                    align_before_front=align_before_front,
-                                                                    interpolation_mod_chunk=interpolation_mod_chunk, # in order to get a multiple of the input number (ex: videoMae chunk is 16, so we need to set this to 16)
-                                                                    extra_landmark_smoothing=None)
-    except extractor.DetectionError as e:
-      print(f'Error in {video_path}: {e}')
-      with open(log_error_path,'a') as f:
-        f.write(f'{video_path}: {e}\n')
-        f.write(f'  list no detection: {e.list_no_detection_idx}\n')
-      continue
-    
-    if generate_video:
-      sample_id = os.path.split(video_path)[-1] # got sampleid.mp4
-      folder_id = Path(video_path).parts[-2]
-      out_path = os.path.join(path_folder_output,folder_id,sample_id)
-      if global_path:
-        out_path = GLOBAL_PATH.get_global_path(out_path)
-      print(f'length of list frame: {len(dict_result_frontalization["list_frontalized_frame"])}')
-      heights, widths = check_image_size(dict_result_frontalization['list_frontalized_frame'])
-      
-      if heights['max'] != heights['min'] or widths['max'] != widths['min']:
-        resize_dim = (heights['max'], widths['max'])
-      else:
-        resize_dim = None
-      if resize_dim is not None:
-        print(f'Resize dim: {resize_dim}')
-      tools.generate_video_from_list_frame(list_frame=dict_result_frontalization['list_frontalized_frame'],
-                                           resize=resize_dim,
-                                          path_video_output=out_path)
-      print(f'Video frontalized: {out_path}')
 
-    count += 1
-    end = time.time()
-    print(f'count: {count}/{len(list_video_path)}')
-    print(f'Elapsed time: {int((end-start)/60/60)} h {int((end-start)/60%60)} m {int((end-start)%60)} s')
-    predicted_time = ((end-start)/count*(len(list_video_path)-count))
-    print(f'Time to end : {int(predicted_time/60/60)} h {int(predicted_time/60%60)} m {int(predicted_time%60)} s\n')
-  
-  
-def check_image_size(input_video_frames):
-  heights = {'max':0,'min':input_video_frames[0].shape[0]}
-  widths = {'max':0,'min':input_video_frames[0].shape[1]}
-  for frame in input_video_frames:
-    height, width, _ = frame.shape
-    if height > heights['max']:
-      heights['max'] = height
-    if width > widths['max']:
-      widths['max'] = width
-    if height < heights['min']:
-      heights['min'] = height
-    if width < widths['min']:
-      widths['min'] = width
-  return heights, widths
+  cfg = {
+    'path_folder_output': path_folder_output,
+    'generate_video': generate_video,
+    'log_error_path': log_error_path,
+    'only_oval': only_oval,
+    'align_before_front': align_before_front,
+    'interpolation_mod_chunk': interpolation_mod_chunk,
+    'global_path': global_path
+  }
+
+  Path(path_folder_output).mkdir(parents=True, exist_ok=True)
+
+  manager = Manager()
+  shared_dict = manager.dict()
+
+  pool = mp.Pool(processes=workers,
+                 initializer=worker_init,
+                 initargs=(ref_landmarks, cfg, shared_dict))
+
+  results_iter = pool.imap_unordered(process_one_video, list_video_path)
+
+  success = 0
+  errors = 0
+  with tqdm.tqdm(total=len(list_video_path),
+                 desc='Frontalizing videos',
+                 unit='video') as pbar:
+    for res in results_iter:
+      pbar.update(1)
+      if res.get('ok'):
+        success += 1
+      else:
+        errors += 1
+
+  pool.close()
+  pool.join()
+  with open(log_error_path, 'w') as f:
+    for key in shared_dict.keys():
+      f.write(f'{key}: {shared_dict[key]}\n')
+  print(f'\nDone. Success: {success}, Errors: {errors}')
 
 if __name__ == '__main__':
-  def read_ltv(ltv):
-    list_video = []
-    with open(args.ltv[0],'r') as f:
-      list_video = f.readlines()
-    ltv = [video.strip() for video in list_video]
-    return ltv
-  
-  def set_global_args(args):
+  def read_ltv(ltvfile):
+    with open(ltvfile, 'r') as f:
+      return [line.strip() for line in f.readlines()]
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--g_path', action='store_true', help='Use GLOBAL_PATH mappings')
+  parser.add_argument('--gv', action='store_true', help='Generate video')
+  parser.add_argument('--vfp', type=str, default=os.path.join('partA','video','video'),
+                      help='Path to video folder')
+  parser.add_argument('--csv', type=str,
+                      default=os.path.join('partA','starting_point','samples_exc_no_detection.csv'),
+                      help='Path to csv file')
+  parser.add_argument('--pfo', type=str,
+                      default=os.path.join('partA','video','video_frontalized'),
+                      help='Path to folder video output')
+  parser.add_argument('--ltv', type=str, default=None, nargs='+',
+                      help='List of target video. Ex: video_name.txt ')
+  parser.add_argument('--prl', type=str,
+                      default=os.path.join('partA','video','mean_face_landmarks_per_subject','all_subjects_mean_landmarks.pkl'),
+                      help='Path to reference landmarks')
+  parser.add_argument('--from_', type=int, default=None,
+                      help='From index (included)')
+  parser.add_argument('--to_', type=int, default=None,
+                      help='To index (excluded)')
+  parser.add_argument('--only_oval', action='store_true',
+                      help='Extract only face oval from the video')
+  parser.add_argument('--interpolation_mod_chunk', nargs='*', default=None,
+                      help='[mod, chunk]')
+  parser.add_argument('--align_before_front', action='store_true',
+                      help='Align the face before frontalization')
+  parser.add_argument('--workers', type=int, default=4,
+                      help='Number of worker processes for parallel execution')
+  args = parser.parse_args()
+
+  if args.ltv and args.ltv[0].endswith('.txt'):
+    args.ltv = read_ltv(args.ltv[0])
+
+  if args.g_path:
     os.chdir(GLOBAL_PATH.NAS_PATH)
     args.csv = GLOBAL_PATH.get_global_path(args.csv)
     args.pfo = GLOBAL_PATH.get_global_path(args.pfo)
     args.prl = GLOBAL_PATH.get_global_path(args.prl)
-    # args.log_er_p = GLOBAL_PATH.get_global_path(args.log_er_p)
     print(f'csv path: {args.csv}')
     print(f'path folder output: {args.pfo}')
     print(f'path ref landmarks: {args.prl}')
-    
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--g_path',action='store_true', help='Add /equilibrium/fvilli/PainAssessmentVideo to all paths')
-  parser.add_argument('--gv', action='store_true', help='Generate video')
-  parser.add_argument('--csv', type=str, default=os.path.join('partA','starting_point','samples_exc_no_detection.csv'), help='Path to csv file')
-  parser.add_argument('--pfo', type=str, default=os.path.join('partA','video','video_frontalized'), help='Path to folder video output')
-  parser.add_argument('--ltv', type=str, default=None,nargs='+', help='List of target video. Ex: video_name.txt ')
-  parser.add_argument('--prl', type=str, default=os.path.join('partA','video','mean_face_landmarks_per_subject','all_subjects_mean_landmarks.pkl'), help='Path to reference landmarks')
-  parser.add_argument('--from_', type=int, default=None, help='Frontalize video from index (included)')
-  parser.add_argument('--to_', type=int, default=None, help='Frontalize video untill the index (excluded). Set None to get all video')
-  # parser.add_argument('--log_er_p', type=str, default=os.path.join('partA','video','video_frontalized'), help='Path to log error file')
-  parser.add_argument('--vfp', type=str, default=os.path.join('partA','video','video'), help='Path to video folder. Default to partA/video/video')
-  parser.add_argument('--only_oval', action='store_true', help='Extract only face oval from the video')
-  parser.add_argument('--interpolation_mod_chunk',nargs='*', default=None, help='mod can be spread_linearly or mirror_start_video, chunk is the chunk size in order to get a multiple number of frames according to chunk')
-  parser.add_argument('--align_before_front', action='store_true', help='Align the face before frontalization')
-  # sripts example: python3 extract_video_frontalized.py --g_path --gv --csv partA/starting_point/samples_exc_no_detection.csv --pfo partA/video/video_frontalized_new --prl partA/video/mean_face_landmarks_per_subject/all_subjects_mean_landmarks.pkl --from_ 0 --to_ 10
-  args = parser.parse_args()
-  if args.ltv and args.ltv[0].endswith('.txt'):
-    args.ltv = read_ltv(args.ltv)
-    
-  if args.g_path:
-    set_global_args(args)
-  dict_args = vars(args)
-  if dict_args['interpolation_mod_chunk'] and len(dict_args['interpolation_mod_chunk']) != 2:
-    print(f'interpolation_mod_chunk must have 2 elements. mod can be spread_linearly or mirror_start_video, chunk is the chunk size in order to get a multiple number of frames according to chunk')
-  for key, value in dict_args.items():
-    print(f'{key}: {value}')
-    
+
+  if args.interpolation_mod_chunk and len(args.interpolation_mod_chunk) != 2:
+    raise SystemExit('interpolation_mod_chunk must have 2 elements: mod and chunk')
+
+  interp = None
+  if args.interpolation_mod_chunk:
+    interp = [args.interpolation_mod_chunk[0], int(args.interpolation_mod_chunk[1])]
+
+  print("Running with args:")
+  for k, v in vars(args).items():
+    print(f'  {k}: {v}')
+
   main(generate_video=args.gv,
        csv_path=args.csv,
        path_folder_output=args.pfo,
@@ -161,7 +238,6 @@ if __name__ == '__main__':
        log_error_path=args.pfo,
        only_oval=args.only_oval,
        align_before_front=args.align_before_front,
-       interpolation_mod_chunk=[args.interpolation_mod_chunk[0],int(args.interpolation_mod_chunk[1])] if args.interpolation_mod_chunk else None,
-       video_folder_path=args.vfp)
-  
-  
+       interpolation_mod_chunk=interp,
+       video_folder_path=args.vfp,
+       workers=args.workers)
