e73551bb8b60c2a32384401f2e7b488f14258654
villifCoder559 <francesco.villi@gmail.com>
2025-11-18 17:55:42 +0100

    fix space

--- custom/backbone.py
+++ custom/backbone.py
@@ -10,6 +10,7 @@ from VideoMAEv2.models.modeling_finetune import (
   vit_base_patch16_224,
   vit_giant_patch14_224
 )
+
 from VideoMAEv2.models.modeling_pretrain import pretrain_videomae_giant_patch14_224, pretrain_videomae_base_patch16_224,pretrain_videomae_small_patch16_224
 from transformers import ViTFeatureExtractor, ViTModel
 from custom.helper import MODEL_TYPE, ModelTypeEntry
