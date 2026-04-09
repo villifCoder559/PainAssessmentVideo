9ac7b03618efb21a3828677219c0439684c5a0ef
VillifCoder559 <francesco.villi@gmail.com>
2026-04-09 12:23:24 +0000

    fix loss plot range

--- new_plot_res_from_server.py
+++ new_plot_res_from_server.py
@@ -548,8 +548,8 @@ def plot_losses(data, run_output_folder, test_id, loss_plot_type,additional_info
         # add test_id in dict_to_string
         dict_to_string += f'\nTest ID: {test_id}'
         dict_to_string += f'\nfold_subfold: {key.split("_")[0]}_{key.split("_")[-1]}'
-        y_lim_loss = 2.5
-        step_lim = 0.25
+        y_lim_loss = 2.5 if 'partA' in "".join(data['config']['path_csv_dataset']).lower() else 9.1
+        step_lim = 0.25 if 'partA' in "".join(data['config']['path_csv_dataset']).lower() else 1
         if isinstance(data['config']['criterion'],torch.nn.MSELoss):
           y_lim_loss = 15.1
           step_lim = 3
